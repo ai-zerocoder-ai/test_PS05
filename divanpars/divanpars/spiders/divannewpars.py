bc0ae55c363223ed33f29cb57774f80471a4faef
ai-zerocoder-ai <ai.zerocoder.ai@gmail.com>
2024-11-24 00:16:05 +0300

    Update divannewpars.py

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -10,7 +10,7 @@ class DivannewparsSpider(scrapy.Spider):
         divans = response.css("div._Ud0k")
         for divan in divans:
             yield {
-                'name' : divan.css('div.LsooF span::text').get(),
-                'price' : divan.css('div.pY3d2 span::').get(),
+                'name' : divan.css('div.lsooF span::text').get(),
+                'price' : divan.css('div.pY3d2 span::text').get(),
                 'url' : divan.css('a').attrib['href']
             }
