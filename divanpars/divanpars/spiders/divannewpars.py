4cd0aa55bcd1290469813716bbbbfea06e6a77e5
ai-zerocoder-ai <ai.zerocoder.ai@gmail.com>
2024-11-25 22:18:57 +0300

    update pars .csv

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -1,16 +1,21 @@
 import scrapy
-
-
+import csv
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
-    allowed_domains = ["https://divan.ru"]
+    allowed_domains = ["divan.ru"]
     start_urls = ["https://www.divan.ru/sankt-peterburg/category/svet"]
 
     def parse(self, response):
         divans = response.css("div._Ud0k")
-        for divan in divans:
-            yield {
-                'name' : divan.css('div.lsooF span::text').get(),
-                'price' : divan.css('div.pY3d2 span::text').get(),
-                'url' : divan.css('a').attrib['href']
-            }
+
+        with open('divan.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['название', 'цена', 'ссылка'])
+
+            for divan in divans:
+                name = divan.css("div.lsooF span::text").get()
+                price = divan.css("div.pY3d2 span::text").get()
+                relative_url = divan.css("a::attr(href)").get()
+                url = response.urljoin(relative_url)
+
+                writer.writerow([name, price, url])
